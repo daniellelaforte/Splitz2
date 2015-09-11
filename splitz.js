ee9e4d3fb668a510c47fb7eb33a1bd6e1eaf3a7e
Student <student@Mac-mini-31.local>
2015-09-11 17:22:50 -0600

    fixed the addition

--- splitz.js
+++ splitz.js
@@ -3,18 +3,20 @@ angular.module("bear", []);
 angular.module("bear").controller("polarbearcontroller", ["$scope", function($scope){
 	$scope.message = "Add another price";
 	//$scope.classname="forthehiddenform";
-	$scope.prices=[{amt:0}]
-	$scope.showthis=false;
+	$scope.prices=[{amt:""}]
 	$scope.counter=0;
-	$scope.showthis1=false;
+	$scope.total=0;
 
 	
 	$scope.Submit = function() {
 			//event.preventDefault(); //don't need this - angular does this automatically
-		   for (var i; i<prices.length; i++){
-		   	    if ($scope.prices[i].amt!="null")
-					$scope.total+=Number($scope.prices[i].amt) + Number($scope.tax);
+		   for (var i=0; i<$scope.prices.length; i++){
+	   	    $scope.prices[i].amt = $scope.prices[i].amt || 0
+					$scope.total+=parseFloat($scope.prices[i].amt)
+				
 		   }
+		  	$scope.total+=parseFloat($scope.tax);
+		  
     }
 
     $scope.Show = function() {
@@ -25,7 +27,7 @@ angular.module("bear").controller("polarbearcontroller", ["$scope", function($sc
 		    		$scope.showthis1=true; 
 
 		    	$scope.counter++;  
-		    	$scope.prices.push({amt:0}); 
+		    	$scope.prices.push({amt:""}); 
 
     }
 
